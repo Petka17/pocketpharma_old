184c15a3df5dab8b508cfb9a6292da66531fe3df
Petr Klimenko <petrklim@yandex.ru>
2013-11-04 10:10:23 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pocketpharma::Application.config.secret_key_base = 'a99ac612f7b934384944eeb724e93c3c6b93993d5d8450adeb3f3fb176c51a4362a2366066183a748080c6763a88cdfd1c03260ca321852bba4aa292bd780260'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pocketpharma::Application.config.session_store :cookie_store, key: '_pocketpharma_session'
