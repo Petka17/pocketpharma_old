291e873096b2b3a5f4230637a46cc6e0906294b1
Petr Klimenko <petrklim@yandex.ru>
2014-01-19 03:05:21 +0400

    Sync rls upload

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,7 +7,8 @@ class StaticPagesController < ApplicationController
 	end
 
 	def rls_import
-  	RlsProductWorker.perform_async(params[:file].path)
+  	# RlsProductWorker.perform_async(params[:file].path)
+  	import_rls_product(params[:file].path)
 		redirect_to manage_path
 	end
 
@@ -26,4 +27,20 @@ class StaticPagesController < ApplicationController
 		redirect_to manage_path		
 	end
 	
+	private
+	  def import_rls_product(path)
+		  spreadsheet = Roo::Excel.new(path, nil, :ignore)
+		  header = spreadsheet.row(1)
+			inserts = []
+
+		  (2..spreadsheet.last_row).each do |i|	    
+		    row = Hash[[header, spreadsheet.row(i)].transpose]
+		    ean = row["ean"].to_s.empty? ? "NULL" : row["ean"].to_i
+		    inserts.push "(#{row["code"]},\'#{row["name"]}\',\'#{row["category"]}\',\'#{row["type"]}\',\'#{row["form"]}\',\'#{row["dose"]}\',\'#{row["pack"]}\',\'#{row["company"]}\',\'#{row["country"]}\',\'#{row["inn"]}\',#{ean})"
+		  end
+
+			sql = "INSERT INTO rls_products (code, name, category, product_group_type, product_form, dose, pack, company, country, inn, ean) VALUES #{inserts.join(", ")}"
+
+	    ActiveRecord::Base.connection.execute sql
+		end
 end
